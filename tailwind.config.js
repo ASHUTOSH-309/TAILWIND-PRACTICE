f020cfa789f4eae3b2a0f156d5816672e6487d25
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-23 00:27:12 +0530

    Tailwind css configured into the project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+//  "./src/**/*.{js,ts,jsx,tsx}", 
+//In my src folder anything(file) ending with any of the above extension then please look for it
+// it might contain the tailwind logic
