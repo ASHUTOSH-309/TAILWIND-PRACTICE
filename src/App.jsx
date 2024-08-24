4ac2a3befb9c6f9f9cbb0b87fee9d906ef3d2a15
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-24 17:18:13 +0530

    dividing screen into 1,2,4 parts based on md,lg breakpoint

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <div className='grid grid-cols-1  md:grid-cols-3'> \
+      <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-4'> 
         {/* on screens with width<768 it will be a single column grid
           while on screens >= 768 px it will be a 3 column grid
       
