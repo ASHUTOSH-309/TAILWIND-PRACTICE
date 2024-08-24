0a7475efa115a30a182159847148baa043325828
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-25 00:06:34 +0530

    Configured Flowbite into the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    './node_modules/flowbite-react/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [    require('flowbite/plugin'),]
 }
-
 //  "./src/**/*.{js,ts,jsx,tsx}", 
 //In my src folder anything(file) ending with any of the above extension then please look for it
 // it might contain the tailwind logic
