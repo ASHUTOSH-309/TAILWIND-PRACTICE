baf31c314a13cdc565ccc24ba9e02350f968c71f
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-25 00:29:13 +0530

    An entirely different component can be rendered on showing Alerts using additionalContent prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import 'tailwindcss/tailwind.css';   // Import Tailwind CSS
 import 'flowbite/dist/flowbite.css'; // Import Flowbite CSS
-
-
-import { useState } from 'react'
-
 import './App.css'
-import { RevenueCard } from './components/RevenueCard'
 import { Success } from './components/Success'
 import { Failure } from './components/Faliure';
 import { Info } from './components/Info';
 import { Warning } from './components/Warning';
+import { AdditionalComponent } from './components/AdditionalComponent';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div>
@@ -20,6 +16,7 @@ function App() {
       <Failure />
       <Info />
       <Warning />
+      <AdditionalComponent />
     </div>
 
   )
